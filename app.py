f26ba9319ae95270c33d967e71285ec48fc1b36f
LuisAngel0621 <luisangel.taipe@unmsm.edu.pe>
2024-05-31 09:01:59 -0500

    Prueba de conexion a BD

--- app.py
+++ app.py
@@ -1,26 +1,33 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-from flask_marshmallow import Marshmallow
-from Config import Config
+from routes.GuardarDatos import usuario
+from routes.GuardarRespuestas import respuestas
+from config import DATABASE_CONNECTION_URI
+from flask_cors import CORS
+from db import db
+from ma import ma
 
-db = SQLAlchemy()
-ma = Marshmallow()
+app = Flask(__name__)
+        
+CORS(app, resources={r"/*": {"origins": "*"}})
+CORS(app, supports_credentials=True)
+app.secret_key = 'clavesecreta123'
 
-def create_app():
-    app = Flask(__name__)
-    app.config.from_object(Config)
+app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_CONNECTION_URI
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config["SQLALCHEMY_POOL_SIZE"] = 20
+app.config["SQLALCHEMY_POOL_TIMEOUT"] = 30
+app.config["SQLALCHEMY_POOL_RECYCLE"] = 1800
 
-    db.init_app(app)
-    ma.init_app(app)
+# no cache
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 
-    with app.app_context():
-        from routes import todo_bp
-        app.register_blueprint(todo_bp, url_prefix='/api')
+db.init_app(app)
+ma.init_app(app)
+app.register_blueprint(usuario)
+app.register_blueprint(respuestas)
 
-        db.create_all()
-
-    return app
+with app.app_context():
+    db.create_all()
 
 if __name__ == '__main__':
-    app = create_app()
-    app.run(debug=True)
+    app.run(debug=True, host="0.0.0.0")
