c340d357ac65b8672378a3adaedc81e49b062862
LuisAngel0621 <luisangel.taipe@unmsm.edu.pe>
2024-06-19 18:52:21 -0500

    Actualización de servicio

--- routes/RegistrarUsuario.py
+++ routes/RegistrarUsuario.py
@@ -3,6 +3,7 @@ from flask import request, make_response
 from flask import jsonify
 from models.Usuario import Usuario
 from models.UsuarioTipo import UsuarioTipo
+from schemas.Usuario_schema import Usuario_Schema
 from werkzeug.security import generate_password_hash
 import secrets
 import string
@@ -36,16 +37,21 @@ def Guardar_datos():
 def Buscar_usuario(correoinstitucional):
     usuario = Usuario.query.filter_by(correoinstitucional = correoinstitucional).first()
 
-    # Verificar si el usuario fue encontrado
-    if usuario:
-        # Preparar la respuesta con la información del usuario
-        usuario_data = {
-            'id': usuario.id_usu
+    if not usuario:
+        data = {
+            'message': 'Usuario no encontrado',
+            'status': 404
         }
-        return make_response(jsonify(usuario_data), 200)
-    else:
-        # Si no se encuentra el usuario, devolver un mensaje de error
-        return make_response(jsonify({'message': 'Usuario no encontrado'}), 404)
+        return make_response(jsonify(data), 404)
+    
+    result = Usuario_Schema.dump(usuario)
+
+    data = {
+        'message': 'Usuario encontrado',
+        'status': 200,
+        'data': result       
+    }
+    return make_response(jsonify(data), 200)
 
 @usuario.route('/EnviarContrasenia', methods = ['POST'])
 def Generar_contrasenia():
