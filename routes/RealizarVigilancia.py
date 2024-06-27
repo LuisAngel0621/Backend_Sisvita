51d364c034ba775862f6c07693e9ab873ff5db80
EduAB <armas.eduardo2000@gmail.com>
2024-06-27 02:34:12 -0500

    v act
    comit de vista

--- routes/RealizarVigilancia.py
+++ routes/RealizarVigilancia.py
@@ -5,20 +5,28 @@ from flask import jsonify
 from models.Diagnostico import Diagnostico
 from models.Nivel import Nivel
 from schemas.Nivel_Schema import Niveles_Schema
-from schemas.Diagnostico_Schema import Diagnosticos_Schema
+from schemas.Diagnostico_Schema import Diagnostico_Schema
 from db import db
 
 vigilancia = Blueprint('vigilancia', __name__)
 
-@vigilancia.route('/RealizarVigilancia', methods = ['GET'])
-def TestRealizados():
-    usuarios_eval = Diagnostico.query.all()
-    resultado = Diagnosticos_Schema.dump(usuarios_eval)
-    response ={
-       'success': True,
-        'data': resultado
-    }    
-    return make_response(jsonify(response),201)
+@vigilancia.route('/RealizarVigilancia/<id>', methods = ['GET'])
+def TestRealizados(id):
+    Evatest = Diagnostico.query.filter_by(id_diag=id).first()
+    if Evatest:
+        res = Diagnostico_Schema.dump(Evatest)
+        response ={
+            'success': True,
+            'data': res
+        }    
+        return make_response(jsonify(response),200)
+    else:
+        response ={
+            'success': True,
+            'message': 'Diagnostico no encontrado'
+        }
+        return make_response(jsonify(response), 404)
+
 
 @vigilancia.route('/ObtenerNivel', methods = ['GET'])
 def ObtenerNivel():
