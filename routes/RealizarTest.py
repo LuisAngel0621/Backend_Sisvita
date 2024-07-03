6af6626c426d622ce6bc0b14ab7ca9c4c948a0b5
LuisAngel0621 <luisangel.taipe@unmsm.edu.pe>
2024-07-03 18:14:13 -0500

    Actualizacion de servicios

--- routes/RealizarTest.py
+++ routes/RealizarTest.py
@@ -7,9 +7,11 @@ from models.Diagnostico import Diagnostico
 from models.Respuestas import Respuestas
 from models.Preguntas import Preguntas
 from models.Escala import Escala
+from models.Nivel import Nivel
 from schemas.Escala_Schema import Escalas_Schema
 from schemas.Respuestas_Schema import Respuestas_Schema
 from schemas.Pregunta_Schema import Preguntas_Schema
+from schemas.Nivel_Schema import Niveles_Schema
 from schemas.UsuarioTest_schema import Usuario_Test_Schema
 from db import db
 import time
@@ -32,7 +34,9 @@ def Guardar_Respuestas():
         new_test = UsuarioTest(id_test = id_test,id_paciente=id_paciente,id_escala = id_escala, id_respuestas=id_respuestas, id_preguntas=id_preguntas)
         db.session.add(new_test)
     db.session.commit()
-    return make_response(jsonify({'message': 'Registro de Test exitoso'}), 201)
+
+    id_user_test = db.session.query(UsuarioTest).order_by(UsuarioTest.id_user_test.desc()).first().id_user_test
+    return make_response(jsonify({'message': 'Registro de Test exitoso', 'id_user_test': id_user_test}), 201)
 
 
 @respuestas.route('/ObtenerPreguntas', methods = ['GET'])
@@ -74,6 +78,18 @@ def obtener_escala():
 
     return make_response(jsonify(data),201)
 
+@respuestas.route("/ObtenerNivel", methods = ['GET'])
+def obtener_nivel():
+    niveles = Nivel.query.all()
+    nivelesSchema = Niveles_Schema.dump(niveles)
+
+    data = {
+        'niveles': nivelesSchema,
+        'status': 201
+    }
+
+    return make_response(jsonify(data),201)
+
 @respuestas.route('/SumaPuntaje/<int:id_paciente>', methods = ['GET'])
 def suma_puntaje(id_paciente):
     suma_id_escala = db.session.query(db.func.sum(UsuarioTest.id_escala)).filter_by(id_paciente=id_paciente).scalar()
@@ -97,8 +113,11 @@ def resultado_test():
     db.session.add(new_diagnostico)
     db.session.commit()
 
+    id_diag = db.session.query(Diagnostico).order_by(Diagnostico.id_diag.desc()).first().id_diag
+
     data = {
-        'success': True
+        'success': True,
+        'id_diag': id_diag
     }
     
     return make_response(jsonify(data),201)
